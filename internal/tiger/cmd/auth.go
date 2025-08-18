a3d18fef4b8a0dfedbb96a65c2e576a2ecc2ee89
Nathan Cochran <nathanjcochran@gmail.com>
2025-08-18 17:57:14 -0400

    Move GraphQL code into separate file

--- internal/tiger/cmd/auth.go
+++ internal/tiger/cmd/auth.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -115,6 +114,9 @@ Examples:
 
 			c := &loginCmd{
 				cfg: cfg,
+				graphql: &GraphQLClient{
+					URL: cfg.GatewayURL,
+				},
 				out: cmd.OutOrStdout(),
 			}
 
@@ -243,8 +245,9 @@ func buildAuthCmd() *cobra.Command {
 }
 
 type loginCmd struct {
-	cfg *config.Config
-	out io.Writer
+	cfg     *config.Config
+	graphql *GraphQLClient
+	out     io.Writer
 }
 
 func (c *loginCmd) loginWithOAuth() (string, string, error) {
@@ -428,7 +431,7 @@ func (c *loginCmd) createOAuthCallbackHandler(oauthCfg oauth2.Config, expectedSt
 // selectProjectID prompts the user to select a project if multiple are available
 func (c *loginCmd) selectProjectID(accessToken string) (string, error) {
 	// First, get the list of projects the user has access to
-	projects, err := c.getUserProjects(accessToken)
+	projects, err := c.graphql.getUserProjects(accessToken)
 	if err != nil {
 		return "", fmt.Errorf("failed to get user projects: %w", err)
 	}
@@ -515,13 +518,13 @@ func (m projectSelectModel) View() string {
 // createAPIKey creates an API key for the selected project
 func (c *loginCmd) createAPIKey(accessToken, projectID string) (string, error) {
 	// Get user information for PAT name
-	user, err := c.getUser(accessToken)
+	user, err := c.graphql.getUser(accessToken)
 	if err != nil {
 		return "", fmt.Errorf("failed to get user info: %w", err)
 	}
 
 	// Create a PAT record for this project
-	patRecord, err := c.createPATRecord(accessToken, projectID, c.buildPATName(user))
+	patRecord, err := c.graphql.createPATRecord(accessToken, projectID, c.buildPATName(user))
 	if err != nil {
 		return "", fmt.Errorf("failed to create PAT record: %w", err)
 	}
@@ -544,162 +547,6 @@ func (c *loginCmd) buildPATName(user *User) string {
 	} else {
 		return "TigerCLI"
 	}
-}
-
-// GetAllProjectsResponse represents the response from the getAllProjects query
-type GetAllProjectsResponse struct {
-	Data struct {
-		GetAllProjects []Project `json:"getAllProjects"`
-	} `json:"data"`
-}
-
-// Project represents a project from the GraphQL API
-type Project struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
-// getUserProjects fetches the list of projects the user has access to
-func (c *loginCmd) getUserProjects(accessToken string) ([]Project, error) {
-	query := `
-		query GetAllProjects {
-			getAllProjects {
-				id
-				name
-			}
-		}
-	`
-
-	response, err := makeGraphQLRequest[GetAllProjectsResponse](c.cfg.GatewayURL, accessToken, query, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return response.Data.GetAllProjects, nil
-}
-
-// GetUserResponse represents the response from the getUser query
-type GetUserResponse struct {
-	Data struct {
-		GetUser User `json:"getUser"`
-	} `json:"data"`
-}
-
-// User represents a user from the GraphQL API
-type User struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-// getUser fetches the current user's information
-func (c *loginCmd) getUser(accessToken string) (*User, error) {
-	query := `
-		query GetUser {
-			getUser {
-				id
-				name
-				email
-			}
-		}
-	`
-
-	response, err := makeGraphQLRequest[GetUserResponse](c.cfg.GatewayURL, accessToken, query, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return &response.Data.GetUser, nil
-}
-
-// CreatePATRecordResponse represents the response from the createPATRecord mutation
-type CreatePATRecordResponse struct {
-	Data struct {
-		CreatePATRecord PATRecordResponse `json:"createPATRecord"`
-	} `json:"data"`
-}
-
-// PATRecordResponse represents the response from creating a PAT record
-type PATRecordResponse struct {
-	ClientCredentials struct {
-		AccessKey string `json:"accessKey"`
-		SecretKey string `json:"secretKey"`
-	} `json:"clientCredentials"`
-}
-
-// createPATRecord creates a new PAT record for the given project
-func (c *loginCmd) createPATRecord(accessToken, projectID, patName string) (*PATRecordResponse, error) {
-	query := `
-		mutation CreatePATRecord($input: CreatePATRecordInput!) {
-			createPATRecord(createPATRecordInput: $input) {
-				clientCredentials {
-					accessKey
-					secretKey
-				}
-			}
-		}
-	`
-
-	variables := map[string]any{
-		"input": map[string]any{
-			"projectId": projectID,
-			"name":      patName,
-		},
-	}
-
-	response, err := makeGraphQLRequest[CreatePATRecordResponse](c.cfg.GatewayURL, accessToken, query, variables)
-	if err != nil {
-		return nil, err
-	}
-
-	return &response.Data.CreatePATRecord, nil
-}
-
-// makeGraphQLRequest makes a GraphQL request to the API using generics
-func makeGraphQLRequest[T any](gatewayURL, accessToken, query string, variables map[string]any) (*T, error) {
-	requestBody := map[string]any{
-		"query": query,
-	}
-	if variables != nil {
-		requestBody["variables"] = variables
-	}
-
-	jsonBody, err := json.Marshal(requestBody)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal GraphQL request: %w", err)
-	}
-
-	queryURL := gatewayURL + "/query"
-	req, err := http.NewRequest("POST", queryURL, strings.NewReader(string(jsonBody)))
-	if err != nil {
-		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to make GraphQL request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("GraphQL request failed with status %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read GraphQL response: %w", err)
-	}
-
-	var response T
-	if err := json.Unmarshal(body, &response); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal GraphQL response: %w", err)
-	}
-
-	return &response, nil
 }
 
 func openBrowserImpl(url string) error {
---
+++ internal/tiger/cmd/graphql.go
@@ -0,0 +1,173 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// We currently use a few GraphQL endpoints as part of the OAuth login flow
+// because they were already available and they accept the OAuth access token
+// for authentication (whereas savannah-public only accepts the client
+// credentials/API Key).
+type GraphQLClient struct {
+	URL string
+}
+
+// GetAllProjectsResponse represents the response from the getAllProjects query
+type GetAllProjectsResponse struct {
+	Data struct {
+		GetAllProjects []Project `json:"getAllProjects"`
+	} `json:"data"`
+}
+
+// Project represents a project from the GraphQL API
+type Project struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// getUserProjects fetches the list of projects the user has access to
+func (c *GraphQLClient) getUserProjects(accessToken string) ([]Project, error) {
+	query := `
+		query GetAllProjects {
+			getAllProjects {
+				id
+				name
+			}
+		}
+	`
+
+	response, err := makeGraphQLRequest[GetAllProjectsResponse](c.URL, accessToken, query, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Data.GetAllProjects, nil
+}
+
+// GetUserResponse represents the response from the getUser query
+type GetUserResponse struct {
+	Data struct {
+		GetUser User `json:"getUser"`
+	} `json:"data"`
+}
+
+// User represents a user from the GraphQL API
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// getUser fetches the current user's information
+func (c *GraphQLClient) getUser(accessToken string) (*User, error) {
+	query := `
+		query GetUser {
+			getUser {
+				id
+				name
+				email
+			}
+		}
+	`
+
+	response, err := makeGraphQLRequest[GetUserResponse](c.URL, accessToken, query, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response.Data.GetUser, nil
+}
+
+// CreatePATRecordResponse represents the response from the createPATRecord mutation
+type CreatePATRecordResponse struct {
+	Data struct {
+		CreatePATRecord PATRecordResponse `json:"createPATRecord"`
+	} `json:"data"`
+}
+
+// PATRecordResponse represents the response from creating a PAT record
+type PATRecordResponse struct {
+	ClientCredentials struct {
+		AccessKey string `json:"accessKey"`
+		SecretKey string `json:"secretKey"`
+	} `json:"clientCredentials"`
+}
+
+// createPATRecord creates a new PAT record for the given project
+func (c *GraphQLClient) createPATRecord(accessToken, projectID, patName string) (*PATRecordResponse, error) {
+	query := `
+		mutation CreatePATRecord($input: CreatePATRecordInput!) {
+			createPATRecord(createPATRecordInput: $input) {
+				clientCredentials {
+					accessKey
+					secretKey
+				}
+			}
+		}
+	`
+
+	variables := map[string]any{
+		"input": map[string]any{
+			"projectId": projectID,
+			"name":      patName,
+		},
+	}
+
+	response, err := makeGraphQLRequest[CreatePATRecordResponse](c.URL, accessToken, query, variables)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response.Data.CreatePATRecord, nil
+}
+
+// makeGraphQLRequest makes a GraphQL request to the API using generics
+func makeGraphQLRequest[T any](gatewayURL, accessToken, query string, variables map[string]any) (*T, error) {
+	requestBody := map[string]any{
+		"query": query,
+	}
+	if variables != nil {
+		requestBody["variables"] = variables
+	}
+
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal GraphQL request: %w", err)
+	}
+
+	queryURL := gatewayURL + "/query"
+	req, err := http.NewRequest("POST", queryURL, strings.NewReader(string(jsonBody)))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make GraphQL request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GraphQL request failed with status %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read GraphQL response: %w", err)
+	}
+
+	var response T
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal GraphQL response: %w", err)
+	}
+
+	return &response, nil
+}
