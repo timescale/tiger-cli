4197ba9e13766fb16123158d0f804fd2d646881c
Nathan Cochran <nathanjcochran@gmail.com>
2025-10-28 13:48:43 -0400

    Minor cleanup

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -2,6 +2,6 @@
 set -e
 rm -rf completions
 mkdir completions
-for sh in bash zsh fish powershell; do
+for sh in bash zsh fish; do
 	go run ./cmd/tiger completion "$sh" >"completions/tiger.$sh"
 done
